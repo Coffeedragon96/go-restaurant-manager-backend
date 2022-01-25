098a8cd5f48399af425dd300b3ec60eb72120124
Shrey Raval <ravalshrey@gmail.com>
2022-01-25 18:41:40 +0530

    Created baseline for controllers, added funcs

---
+++ controllers/orderController.go
@@ -0,0 +1,27 @@
+package controller
+
+import "github.com/gin-gonic/gin"
+
+func GetOrders() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func GetOrder() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func AddOrder() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func UpdateOrder() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
